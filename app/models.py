b02b1717303027c66a37c8f728c3c44c68cf07b1
Calix Huang <calix.huang1@gmail.com>
2020-04-11 14:09:00 -0700

    made earthquake model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Earthquake(models.Model):
+    date = models.DateField()
+    latitude = models.DecimalField(max_digits=10, decimal_places=5)
+    longitude = models.DecimalField(max_digits=10, decimal_places=5)
+    depth = models.DecimalField(max_digits=10, decimal_places=5)
+    magnitude = models.DecimalField(max_digits=10, decimal_places=5)
+
+    def __str__(self):
+        return self.date
