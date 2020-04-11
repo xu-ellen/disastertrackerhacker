da9ff6f6e945d54fddf5e50a11863953f34107f4
Calix Huang <calix.huang1@gmail.com>
2020-04-11 16:39:01 -0700

    added forest fire model

--- app/models.py
+++ app/models.py
@@ -9,3 +9,15 @@ class Earthquake(models.Model):
 
     def __str__(self):
         return str(self.date)
+
+
+class ForestFire(models.Model):
+    name = models.CharField(max_length=50)
+    year = models.CharField(max_length=10)
+    latitude = models.DecimalField(max_digits=10, decimal_places=5)
+    longitude = models.DecimalField(max_digits=10, decimal_places=5)
+    size = models.DecimalField(max_digits=5, decimal_places=3)
+    size_class = models.CharField(max_length=1)
+    
+    def __str__(self):
+        return self.name
