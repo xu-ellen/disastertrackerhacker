5d6d07fb7bebd9fee3f2ba4ba52ffa331fa4c006
Calix Huang <calix.huang1@gmail.com>
2020-04-12 12:39:40 -0700

    added light mode and geolocation

--- app/views.py
+++ app/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.http import HttpResponse
 from .models import Earthquake, ForestFire, Hurricane
+import numpy as np
 import pandas as pd
+import pickle
+from sklearn.ensemble import RandomForestRegressor
 import datetime
 
 def index(request):
@@ -10,6 +13,15 @@ def index(request):
 
 
 def earthquakes(request):
+    filename = staticfiles_storage.path('finalized_model.sav')
+
+    user_input = [19.246,145.616]  # takes user input
+    user_array = np.asarray(user_input).reshape(1,-1)
+
+    loaded_model = pickle.load(open(filename, 'rb'))
+    predict = loaded_model.predict(user_array)
+    print(predict)
+
     return render(request, "app/earthquakes.html", context={
         "earthquakes": list(Earthquake.objects.all())
     })
