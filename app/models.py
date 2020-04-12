f55f65a32a8059c408aae331351d861a819ae169
Calix Huang <calix.huang1@gmail.com>
2020-04-11 22:10:07 -0700

    added hurricane model

--- app/models.py
+++ app/models.py
@@ -19,3 +19,12 @@ class ForestFire(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Hurricane(models.Model):
+    name = models.CharField(max_length=50)
+    latitude = models.DecimalField(max_digits=15, decimal_places=10)
+    longitude = models.DecimalField(max_digits=15, decimal_places=10)
+
+    def __str__(self):
+        return self.name 
